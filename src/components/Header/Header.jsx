bf3ed274ed4ba097429716be936c929ab26a2d06
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-04-26 12:28:47 +0545

    feat: implement Header and SideDrawer components with responsive layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,101 @@
+import {
+  Box,
+  Flex,
+  Text,
+  Link,
+  Icon,
+  InputGroup,
+  InputRightElement,
+  Input
+  
+} from "@chakra-ui/react";
 import React from "react";
+import { BiSearch } from "react-icons/bi";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { IoCartOutline } from "react-icons/io5";
 
 const Header = () => {
-  return <div>kl</div>;
+  return (
+    <Box
+      minH="90px"
+      borderBottom="1px"
+      borderBottomColor="gray.400"
+      px={{ base: 4, md: 10, lg: 20, xl: "140px" }}
+      py={{ base: 1, md: 2 }}
+    >
+      <Flex
+        align={{
+          base: "center",
+          md: "center",
+          lg: "end",
+        }}
+        justify={{
+          base: "space-between",
+          md: "space-between",
+        }}
+        minH={{
+          base: "90px",
+          md: "90px",
+        }}
+        color="black"
+        flexDir={{
+          base: "column",
+          md: "row",
+        }}
+      >
+        {/* Left - Logo */}
+        <Box flex="1">
+          <Text textStyle="h2" fontWeight="bold">
+            Exclusive
+          </Text>
+        </Box>
+
+        {/* Center - Nav Items */}
+        <Flex flex="2" justify="center" gap={{ base: 4, md: 8 }}>
+          <Link href="#" fontWeight="medium">
+            Home
+          </Link>
+          <Link href="#" fontWeight="medium">
+            Contact
+          </Link>
+          <Link href="#" fontWeight="medium">
+            About
+          </Link>
+          <Link href="#" fontWeight="medium">
+            SignUp
+          </Link>
+        </Flex>
+
+        {/* Right - Search + Wishlist + Cart */}
+        <Flex flex="1" justify="flex-end" align="center" gap={4}>
+          <InputGroup minW="243px" size="md" minH={"38px"}>
+            <Input
+              placeholder="What are you looking for?"
+              bg="gray.100"
+              pl={"20px"}
+              pr={"12px"}
+              py={"8px"}
+              _placeholder={{
+                color: "gray.500",
+                fontSize: "xs",
+                textAlign: "sta",
+              }}
+              _focus={{
+                borderColor: "black",
+                boxShadow: "0 0 0 1px black",
+              }}
+            />
+            <InputRightElement pointerEvents="painted">
+              <BiSearch fontSize={"18px"} color="gray.500" />
+            </InputRightElement>
+          </InputGroup>
+
+          <Icon as={IoIosHeartEmpty} boxSize={6} cursor="pointer" />
+          <Icon as={IoCartOutline} boxSize={6} cursor="pointer" />
+        </Flex>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Header;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import TopNavBar from "./Header/TopNavBar";
-import { Box } from "@chakra-ui/react";
 import Header from "./Header/Header";
+import SideDrawer from "./Header/SideDrawer";
 
 const Layout = () => {
   return (
     <>
       <TopNavBar />
-      <Box width="full" px={{ base: 4, md: 10, lg: 20, xl: 40 }}>
-        <Header />
-      </Box>
+      <Header />
+      <SideDrawer />
     </>
   );
 };
